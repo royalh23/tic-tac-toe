309ae46491b0966b535f6b05cccc4ac7bd03f7ec
Royal <royalhuseyn902@gmail.com>
2022-02-10 18:30:24 +0900

    Added a draw_game_board method to the GameBoard class.The method is empty currently

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,6 +8,16 @@ class TicTacToe
       first_player = FirstPlayer.new(first_player_name, first_player_choice)
       puts 'What is the name of the second player?'
       second_player_name = gets.chomp
+      if first_player_choice == 'X'
+        second_player_choice = 'O'
+      else
+        second_player_choice = 'X'
+      end
+      second_player = SecondPlayer.new(second_player_name, second_player_choice)
+      puts "The second player's choice is '#{second_player_choice}' because" \
+            " of the first player's choice."
+      game_board = GameBoard.new
+      game_board.draw_game_board
       break
     end
   end
@@ -28,6 +38,7 @@ class SecondPlayer
 end
 
 class GameBoard
+  def draw_game_board; end
 end
 
 tic_tac_toe = TicTacToe.new
